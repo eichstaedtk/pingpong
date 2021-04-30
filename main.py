ddb82b9da995749e70ed4a410b8ca251a3375376
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2021-04-30 10:16:36 +0200

    Add Show Game Screen for Ping Pong using PyGame

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def start():
+    pygame.init()
+    ORANGE = (255, 140, 0)
+    ROT = (255, 0, 0)
+    GRUEN = (0, 255, 0)
+    SCHWARZ = (0, 0, 0)
+    WEISS = (255, 255, 255)
+    screen = pygame.display.set_mode((640, 480))
+    pygame.display.set_caption("Ping Pong Game made by @Konrad")
+    game_active = True
+    clock = pygame.time.Clock()
+    run_screen(screen,SCHWARZ,clock, game_active)
+
+
+def run_screen(screen, background_color, clock, game_active):
+    while game_active:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_active = False
+                print("Game over ....")
+
+        screen.fill(background_color)
+
+        pygame.display.flip()
+
+        clock.tick(60)
+
+    pygame.quit()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    start()
